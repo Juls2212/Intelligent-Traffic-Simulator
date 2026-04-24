bd6a0691b686c93c0166f27b597e9bee3b2d3021
Juls <juliana.penafiel@campusucc.edu.co>
2026-04-24 17:50:42 -0500

    consol configuration

---
+++ src/main/java/ConsoleDemo.java
@@ -0,0 +1,26 @@
+import context.TrafficSimulator;
+import server.TrafficConsoleServer;
+
+public class ConsoleDemo {
+    public static void main(String[] args) {
+        TrafficSimulator simulator = new TrafficSimulator();
+        TrafficConsoleServer consoleServer = new TrafficConsoleServer();
+
+        consoleServer.printSnapshot("Estado inicial", simulator);
+
+        simulator.increaseTraffic();
+        consoleServer.printSnapshot("Despues de increaseTraffic()", simulator);
+
+        simulator.reportAccident();
+        consoleServer.printSnapshot("Despues de reportAccident()", simulator);
+
+        simulator.reduceTraffic();
+        consoleServer.printSnapshot("Despues de reduceTraffic()", simulator);
+
+        simulator.clearAccident();
+        consoleServer.printSnapshot("Despues de clearAccident()", simulator);
+
+        simulator.advanceSimulation();
+        consoleServer.printSnapshot("Despues de advanceSimulation()", simulator);
+    }
+}
